acf78b1748b851acca0199d9f37798cee92fb4bd
admin <admin@YD02-05>
2021-05-28 11:53:10 +0900

    5/28

--- petMart/src/com/petMart/common/DAO.java
+++ petMart/src/com/petMart/common/DAO.java
@@ -2,21 +2,20 @@ package com.petMart.common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DAO {
-	
 	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	private String user = "pet";
 	private String passwd = "pet";
-	private String url = "jdbc:oracle:thin@192.168.0.67:1521:xe";
 	public Connection conn;
 	
 	public DAO() {
 		try {
 			Class.forName(driver);
 			conn = DriverManager.getConnection(url, user, passwd);
-		}
-		catch(Exception e) {
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
