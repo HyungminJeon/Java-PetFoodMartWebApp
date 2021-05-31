a4ff773149f00c7ae16263d86d49fe0b2978ec19
admin <admin@YD02-05>
2021-05-31 11:29:43 +0900

    5/31

---
+++ petMart/src/com/petMart/product/web/ProductListPaging.java
@@ -0,0 +1,15 @@
+package com.petMart.product.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.petMart.common.DbCommand;
+
+public class ProductListPaging implements DbCommand {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse res) {
+		return null;
+	}
+
+}
